a5a41fc3da8dae4637041af41987bd50ba2122e5
rjnix <rjnixon123@yahoo.com>
2023-02-02 14:59:26 -0500

    Income sql fix

--- server/routes/income.js
+++ server/routes/income.js
@@ -16,7 +16,7 @@ module.exports.insertIncome = async(req,res) => {
     let email = getEmail(req.headers.authorization).then((email) => {return email;});
     
     var sql = `INSERT INTO Incomes (account_id, gross_pay, pay_day, pay_frequency, user_id) 
-      Values ('${account_id}', '${gross_pay}', '${pay_day}', '${pay_frequency}'
+      Values ('${account_id}', '${gross_pay}', '${pay_day}', '${pay_frequency}',
       (SELECT user_id FROM Users WHERE email = '${await email}'))`;
   
     connection.query(sql, function(err, rows)
