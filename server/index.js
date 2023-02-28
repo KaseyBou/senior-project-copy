b7b60e0f8521ca4241e82441176b4e47973e4f0a
rjnix <rjnixon123@yahoo.com>
2023-02-27 20:10:01 -0500

    changes  Procfile

--- server/index.js
+++ server/index.js
@@ -158,7 +158,7 @@ app.use(express.static(path.join(__dirname, "client")))
 // ...
 // Right before your app.listen(), add this:
 app.get("*", (req, res) => {
-    res.sendFile(path.join(__dirname, "client", "build", "index.html"));
+    res.sendFile(path.join(__dirname, "client", "index.html"));
 });
 
 app.listen(PORT, () => {
