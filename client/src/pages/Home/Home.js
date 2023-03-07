e6f109921dca60923e8fec33db566f9ee893c13c
eshilo-draper <eeshilodraper@email.neit.edu>
2023-03-06 20:01:35 -0500

    updated navbar

--- client/src/pages/Home/Home.js
+++ client/src/pages/Home/Home.js
@@ -22,14 +22,21 @@ const Home = () => {
                         />
                     </div>
                     <div className="row">
-                        <ColumnBox pictureFilePath="/budget-transparent.png" altText="Filler" title="Budget" text="Allows for a seamless experience when setting up a budget. You’ll be able to add budget categories (ex. Car maintenance) and see your overall budget for that current month." 
-                        />
-                        <ColumnBox pictureFilePath="/expenditures-tranparent.png" altText="Filler" title="Expenditures" text="Observe your spending patterns. tools that help you better manage your expenditures, such as the ability to add expenses or bills." 
-                        />
-                        <ColumnBox pictureFilePath="/savings-transparent.png" altText="Filler" title="Savings" text="With the help of our financial planner web tool, keep track of your savings. Your accounts, deposits, and sources of income are all visible here. Your current total savings will be displayed for you." 
-                        />
-                        <ColumnBox pictureFilePath="/income-transparent.png" altText="Filler" title="Income" text="Keep track of all of your sources of income. Here, you can view all of your earnings broken down by date, company, and amount." 
-                        />
+                        <ColumnBox title="Budget" text="Allows for a seamless experience when setting up a budget. You’ll be able to add budget categories (ex. Car maintenance) and see your overall budget for that current month." >
+                            <img src="/budget-transparent.png" alt="Filler"/>
+                        </ColumnBox>
+
+                        <ColumnBox title="Expenditures" text="Observe your spending patterns. tools that help you better manage your expenditures, such as the ability to add expenses or bills.">
+                            <img src="/expenditures-tranparent.png" alt="Filler"/>
+                        </ColumnBox>
+
+                        <ColumnBox title="Savings" text="With the help of our financial planner web tool, keep track of your savings. Your accounts, deposits, and sources of income are all visible here. Your current total savings will be displayed for you.">
+                            <img src="/savings-transparent.png" alt="Filler"/>
+                        </ColumnBox>
+
+                        <ColumnBox title="Income" text="Keep track of all of your sources of income. Here, you can view all of your earnings broken down by date, company, and amount.">
+                            <img src="/income-transparent.png" alt="Filler"/>
+                        </ColumnBox>
                     </div>
                 </div>
             </div>
